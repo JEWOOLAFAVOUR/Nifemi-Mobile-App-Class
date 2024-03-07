13fb3289601d92d1ba0b68b0865d15ed57748938
JEWOOLAFAVOUR <jewoolafavour2020@gmail.com>
2024-03-07 15:32:42 -0800

    new push afresh

--- App.js
+++ App.js
@@ -1,20 +1,32 @@
-import React from 'react';
-import { View, Text } from 'react-native';
-
+import React from "react";
+import { View, Text, StyleSheet, Button, Image } from "react-native";
 
 const App = () => {
   return (
-    <View>
-      <Text>Nifemi</Text>
-      <View>
-        <Text>Favour</Text>
+    <View style={styles.page}>
+      <Image source={require('./assets/image1.jpg')} style={{ height: 200, width: 210, alignSelf: 'center', marginTop: 35, }} />
+      <View style={{ marginTop: 25 }}>
+        <Text style={{ fontSize: 24, color: 'black', fontWeight: "bold", textAlign: 'center' }}>Join Facebook</Text>
+        <Text style={{ color: 'grey', fontSize: 16, textAlign: 'center', marginTop: 6 }}>We'll help you create a new account in a few easy steps</Text>
       </View>
-
-      <View></View>
-      <View></View>
-      <View></View>
+      <View style={{ marginTop: 25, marginBottom: 250 }}>
+        <Button title="Next" />
+      </View>
+      <Text style={{ fontSize: 16, color: 'blue', fontWeight: 'bold', textAlign: 'center', }}>Already have an account</Text>
     </View>
   )
 }
 
 export default App;
+
+
+const styles = StyleSheet.create({
+  page: {
+    flex: 1,
+    backgroundColor: 'white',
+    paddingTop: 40,
+    // paddingLeft: 20,
+    paddingHorizontal: 10,
+  },
+})
+
---
+++ database/Class1.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+
+const App = () => {
+    return (
+        <View>
+            <Text>Nifemi</Text>
+            <View>
+                <Text>Favour</Text>
+            </View>
+
+            <View></View>
+            <View></View>
+            <View></View>
+        </View>
+    )
+}
+
+export default App;
