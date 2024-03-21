e187eba06747b95d5b9a83137d3be90ea8d50906
JEWOOLAFAVOUR <jewoolafavour2020@gmail.com>
2024-03-21 15:13:25 +0100

    new skssk

--- App.js
+++ App.js
@@ -1,28 +1,20 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
-
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import HomeScreen from './screen/HomeScreen'
 import ProfilePage from './screen/ProfilePage'
 
-
 const App = () => {
 
-  const Nifemi = createNativeStackNavigator();
+  const Tab = createBottomTabNavigator();
 
   return (
     <NavigationContainer>
-
-      <Nifemi.Navigator screenOptions={{
-        headerShown: false
-      }} >
-
-        <Nifemi.Screen name='Home' component={HomeScreen} />
-        <Nifemi.Screen name='Profile' component={ProfilePage} />
-
-      </Nifemi.Navigator>
-
+      <Tab.Navigator>
+        <Tab.Screen name='Home' component={HomeScreen} />
+        <Tab.Screen name='Profile' component={ProfilePage} />
+      </Tab.Navigator>
     </NavigationContainer>
   )
 }
@@ -30,3 +22,63 @@ const App = () => {
 export default App
 
 const styles = StyleSheet.create({})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { StyleSheet, Text, View } from 'react-native'
+// import React from 'react'
+// import { NavigationContainer } from '@react-navigation/native'
+// import { createNativeStackNavigator } from '@react-navigation/native-stack'
+
+// import HomeScreen from './screen/HomeScreen'
+// import ProfilePage from './screen/ProfilePage'
+
+
+// const App = () => {
+
+//   const Nifemi = createNativeStackNavigator();
+
+//   return (
+//     <NavigationContainer>
+
+//       <Nifemi.Navigator screenOptions={{
+//         headerShown: false
+//       }} >
+
+//         <Nifemi.Screen name='Home' component={HomeScreen} />
+//         <Nifemi.Screen name='Profile' component={ProfilePage} />
+
+//       </Nifemi.Navigator>
+
+//     </NavigationContainer>
+//   )
+// }
+
+// export default App
+
+// const styles = StyleSheet.create({})
